2107ba600d3a7cd5d1ee7c0c257852ce0f03fd0b
guilherme <mr.soares.guilherme@gmail.com>
2016-11-13 12:24:51 -0200

    Novo algoritmo de preenchimento de matriz seguindo o padrão do mockup feito em sala

--- Fonte/parcial2Battleship/src/Domain/Embarcacao.java
+++ Fonte/parcial2Battleship/src/Domain/Embarcacao.java
@@ -1,9 +1,9 @@
 /*Classe embarcação:
-Nome: {porta-aviões, Encouraçado, fragata, submarino, lancha}
-Tamanho: o numero de espaços ocupados na matriz, {1..5}
-Posição: coordenadas x,y correspondente na matriz
-Vida: mede o numero de vezes que a embarcação ainda pode ser acertada,se 0 então 
-foi destruida.
+ *Nome: {porta-aviões, Encouraçado, fragata, submarino, lancha}
+ *Tamanho: o numero de espaços ocupados na matriz, {1..5}
+ *Posição: coordenadas x,y correspondente na matriz
+ *Vida: mede o numero de vezes que a embarcação ainda pode ser acertada, se 0 então 
+ *foi destruida.
 */
 package Domain;
 
@@ -12,48 +12,93 @@ package Domain;
  * @author Franco_2
  */
 public class Embarcacao {
+    public static final int PORTAAVIAO = 1;
+    public static final int ENCOURACADO = 2;
+    public static final int FRAGATA = 3;
+    public static final int SUBMARINO = 4;
+    public static final int LANCHA = 5;
+    public static final int EMBARCACOES_DIFERENTES = 5;
+    public static final int TOTAL_EMBARCACOES = 14;
+    
     private String nome;
     private int tamanho;
-    private int posicao;
+    private int[] posicao;
     private int vida;
     
-    public Embarcacao portaAvioes(){
-        Embarcacao ship = new Embarcacao();
-        ship.nome = "porta-aviões";
-        ship.tamanho = 5;
-        ship.vida = 5;
-        return ship;        
+    public Embarcacao (int embarc) throws Throwable {
+        if (embarc == PORTAAVIAO) {
+           portaAvioes();
+        } else if (embarc == ENCOURACADO) {
+           encouracado();
+        } else if (embarc == FRAGATA) {
+            fragata();
+        } else if (embarc == SUBMARINO) {
+            submarino();
+        } else if (embarc == LANCHA) {
+            lancha();
+        } else {
+            finalize();
+        }
     }
     
-    public Embarcacao encouracado(){
-        Embarcacao ship = new Embarcacao();
-        ship.nome = "encouraçado";
-        ship.tamanho = 4;
-        ship.vida = 4;
-        return ship;        
+    private void portaAvioes() {
+        this.nome = "porta-aviões";
+        this.tamanho = 5;
+        this.vida = 5;
+        this.posicao = new int[tamanho];
     }
     
-    public Embarcacao fragata(){
-        Embarcacao ship = new Embarcacao();
-        ship.nome = "fragata";
-        ship.tamanho = 3;
-        ship.vida = 3;
-        return ship;        
+    private void encouracado() {
+        this.nome = "encouraçado";
+        this.tamanho = 4;
+        this.vida = 4;
+        this.posicao = new int[tamanho];
     }
     
-    public Embarcacao submarino(){
-        Embarcacao ship = new Embarcacao();
-        ship.nome = "submarino";
-        ship.tamanho = 2;
-        ship.vida = 2;
-        return ship;        
+    private void fragata(){
+        this.nome = "fragata";
+        this.tamanho = 3;
+        this.vida = 3;
+        this.posicao = new int[tamanho];
     }
     
-    public Embarcacao lancha(){
-        Embarcacao ship = new Embarcacao();
-        ship.nome = "lancha";
-        ship.tamanho = 1;
-        ship.vida = 1;
-        return ship;        
+    private void submarino(){
+        this.nome = "submarino";
+        this.tamanho = 2;
+        this.vida = 2;
+        this.posicao = new int[tamanho];
     }
+    
+    private void lancha(){
+        this.nome = "lancha";
+        this.tamanho = 1;
+        this.vida = 1;
+        this.posicao = new int[tamanho];
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public int[] getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(int[] posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
 }
--- Fonte/parcial2Battleship/src/Domain/Tabuleiro.java
+++ Fonte/parcial2Battleship/src/Domain/Tabuleiro.java
@@ -10,19 +10,32 @@ import java.util.Random;
  * @author Franco_2
  */
 public class Tabuleiro {
-    private int matriz[][];
+    private Embarcacao matriz[][];
     private Jogador desafiante;
     private Jogador adversario;
     
-public Tabuleiro(){   
-    imprime(setFrota());
-}//chamada de teste
+    public Tabuleiro() throws Throwable{
+        matriz = new Embarcacao[10][10];
+        //imprime(setFrota());
+        preencheMatriz();
+        imprime2();
+    }//chamada de teste
 
-public int[][] setMatriz(){
-    int[][] m = new int[10][10];
-    return m;
-}//preenchimento da matriz 
-
+    private void imprime2() {
+        System.out.println("Matriz");
+        for (int i=0;i<10;i++){ 
+            System.out.print(i+ " ");
+            for (int j=0;j<10;j++){
+                if (this.matriz[i][j] != null) {
+                    System.out.print(matriz[i][j].getTamanho() + " ");  
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }      
+            System.out.println("");
+        }   
+    }
+    
     public void imprime(int x[][]){
         //mock method
         for (int j=0;j<10;j++){
@@ -33,16 +46,79 @@ public int[][] setMatriz(){
         }   
     }//imprime matriz
     
-    public int sorteia(int i){
+    /**
+     * O método recebe como parâmetro o número máximo que deve ser retornado de modo randômico.
+     */
+    public int sorteia(int max) {
         Random rand = new Random();
-        int r = rand.nextInt(i)+1;
-        return r;  
+        int r = rand.nextInt(max);
+        if (r < 0) {
+            return r*-1;
+        }
+        return r;
     }//retorna numero randomico intervalo i
     
-    public int[][] setFrota() {
-        int tab[][] = setMatriz();
+    private boolean posicoesValidas(int linha, int coluna, int tamanho) {
+        for(int a=0; a<tamanho; a++, coluna++) {
+            if (this.matriz[linha][coluna] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void setEmbarcacao(Embarcacao emb) {
+        int linha=0, coluna=0, controle = emb.getTamanho();
+        boolean posOk = false;
+        linha = sorteia(10);
+        coluna = sorteia(10-emb.getTamanho());
+        imprime2();
+        posOk = posicoesValidas(linha, coluna, emb.getTamanho());
+        do {
+            if (posOk == false) {
+                linha = sorteia(10);
+                coluna = sorteia(10-emb.getTamanho());
+                posOk = posicoesValidas(linha, coluna, emb.getTamanho());
+            } else {
+                this.matriz[linha][coluna] = emb;
+                coluna++;
+                controle--;
+            }
+        } while(controle > 0);
+    }
+    
+    private void preencheMatriz() throws Throwable {
+        int[] controle = {1, 2, 3, 4, 5};
+        
+        int a=0, coluna=0, linha=0, embarc;
+        while (a < Embarcacao.TOTAL_EMBARCACOES) {
+            embarc = sorteia(Embarcacao.EMBARCACOES_DIFERENTES);
+            if (embarc == Embarcacao.PORTAAVIAO && controle[0] != 0) {
+                controle[0]--;
+                setEmbarcacao(new Embarcacao(Embarcacao.PORTAAVIAO));
+            } else if (embarc == Embarcacao.ENCOURACADO && controle[1] != 0) {
+                controle[1]--;
+                setEmbarcacao(new Embarcacao(Embarcacao.ENCOURACADO));
+            } else if (embarc == Embarcacao.FRAGATA && controle[2] != 0) {
+                controle[2]--;
+                setEmbarcacao(new Embarcacao(Embarcacao.FRAGATA));
+            } else if (embarc == Embarcacao.SUBMARINO && controle[3] != 0) {
+                controle[3]--;
+                setEmbarcacao(new Embarcacao(Embarcacao.SUBMARINO));
+            } else if (embarc == Embarcacao.LANCHA && controle[4] != 0) {
+                controle[4]--;
+                setEmbarcacao(new Embarcacao(Embarcacao.LANCHA));
+            }
+            a++;
+        }
+    }
+    
+    public int[][] setFrota() throws Throwable {
+        int tab[][] = new int[10][10];
         int count;
         int i,j;
+        
+        
         
         //setando  1 porta-aviões
         i = sorteia(5);
@@ -138,6 +214,6 @@ public int[][] setMatriz(){
         }
         }
               
-return tab;
+        return tab;
     }
 }
